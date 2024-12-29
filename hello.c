0b83984685b00c950cf429051049265148c4937b
Arnaud Sautaux <arnaud.sautaux@mc-monitoring.com>
2024-12-29 22:46:47 +0100

    added ball

--- hello.c
+++ hello.c
@@ -3,13 +3,26 @@
 int main() {
   // Initialization
 
+  // screen
   const int screenWidth = 800;
   const int screenHeight = 450;
+
+  // Text
   const int fontSize = 20;
   const char *text =
       "Hello the text is perfectly centered, but the rectangle not ...";
   int textWidth;
-  int rectanglePosY = 0;
+
+  // Rectangle
+  int rectangleWidth = 50;
+  int rectangleHeight = 150;
+  int rectanglePositionX = 0;
+  int rectanglePositionY = 0;
+
+  // Ball
+  int ballRadius = 25;
+  int ballPositionX = screenWidth / 2 - ballRadius;
+  int ballPositionY = screenHeight / 2 - ballRadius;
 
   InitWindow(screenWidth, screenHeight, "basic window");
 
@@ -24,11 +37,15 @@ int main() {
     DrawText(text, screenWidth / 2 - textWidth / 2,
              screenHeight / 2 - fontSize / 2, fontSize, LIGHTGRAY);
 
-    DrawRectangle(screenWidth / 2, rectanglePosY, 50, 50, BLACK);
-    rectanglePosY = rectanglePosY + 1;
-    if (rectanglePosY > screenHeight) {
-      rectanglePosY = 0;
+    DrawRectangle(rectanglePositionX, rectanglePositionY, rectangleWidth,
+                  rectangleHeight, BLACK);
+    rectanglePositionY = rectanglePositionY + 1;
+    if (rectanglePositionY > screenHeight) {
+      rectanglePositionY = 0;
     }
+
+    DrawRectangle(ballPositionX, ballPositionY, 2 * ballRadius, 2 * ballRadius,
+                  RED);
 
     EndDrawing();
   }
