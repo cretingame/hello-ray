d09202d80a9e60c1548d6268fad8fa3cf1097a68
Arnaud Sautaux <arnaud.sautaux@mc-monitoring.com>
2024-12-29 19:23:06 +0100

    rectancle added

--- hello.c
+++ hello.c
@@ -6,8 +6,10 @@ int main() {
   const int screenWidth = 800;
   const int screenHeight = 450;
   const int fontSize = 20;
-  const char *text = "Hello this is not perfectly centered ...";
+  const char *text =
+      "Hello the text is perfectly centered, but the rectangle not ...";
   int textWidth;
+  int rectanglePosY = 0;
 
   InitWindow(screenWidth, screenHeight, "basic window");
 
@@ -21,6 +23,12 @@ int main() {
 
     DrawText(text, screenWidth / 2 - textWidth / 2,
              screenHeight / 2 - fontSize / 2, fontSize, LIGHTGRAY);
+
+    DrawRectangle(screenWidth / 2, rectanglePosY, 50, 50, BLACK);
+    rectanglePosY = rectanglePosY + 1;
+    if (rectanglePosY > screenHeight) {
+      rectanglePosY = 0;
+    }
 
     EndDrawing();
   }
